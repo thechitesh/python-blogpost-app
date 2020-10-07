404d05faab57908f936d424e31886b316d8fa13a
chitesh <thechitesh@gmail.com>
2020-10-07 03:32:55 +0200

    added templates and used render_template function

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+posts = [
+
+    {
+        'author': 'Chitesh',
+        'title' : 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'October 7, 2020'  
+    },
+
+    {
+        'author': 'Chandan',
+        'title' : 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 8, 2020'  
+    }
+]
+
 @app.route('/home')
 def hello():
-    return '<h1> Home Page </h2>'
+    return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about():
-    return "<h2> About Page </h2>"
+    return render_template('about.html', title = 'About')
 
 
 if __name__ == '__main__':
