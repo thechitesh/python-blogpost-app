79689939dbaa84e64f5aa9d5360cca4ab9ecd0d1
chitesh <thechitesh@gmail.com>
2020-10-11 17:56:18 +0200

    added login required decorator and next_page redirect from args

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -11,6 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 
 # this import is done here, as there is routes import app from __init__ and this will create cyclic dependency 
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,8 @@
-from flask import  render_template, url_for, flash, redirect
+from flask import  render_template, url_for, flash, redirect, request
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 from flaskblog import app , db, bcrypt
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
 
@@ -56,7 +56,8 @@ def login():
         user = User.query.filter_by(email = form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('homeFunc'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('homeFunc'))
         else:
             flash('Login Unsuccessful. Please check email and password ', 'danger')
 
@@ -67,3 +68,8 @@ def login():
 def logout() :
     logout_user()
     return redirect(url_for('homeFunc'))
+
+@app.route('/account')
+@login_required
+def account() :
+    return render_template('account.html', title='Account')
